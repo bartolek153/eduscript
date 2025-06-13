388c090ffea6938ee4d471c770b60230a5ed1037
Luiz Eduardo Bartolassi <luizekbartolassi@gmail.com>
2025-06-12 23:54:24 -0300

    feat: handle job request caching

--- backend/src/main/java/org/eduscript/controllers/CompileController.java
+++ backend/src/main/java/org/eduscript/controllers/CompileController.java
@@ -5,8 +5,11 @@ import java.util.UUID;
 import org.eduscript.model.CompileRequest;
 import org.eduscript.model.CompileResponse;
 import org.eduscript.model.JobMessage;
+import org.eduscript.model.JobSession;
+import org.eduscript.repositories.JobSessionRepository;
 import org.eduscript.services.JobRequestProducer;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,20 +19,27 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 @RestController
 @RequestMapping("/api/compile")
 public class CompileController {
-    
+
     private final JobRequestProducer jobRequestProducer;
+    private final JobSessionRepository jobSessionRepository;
 
     public CompileController(
-            JobRequestProducer jobRequestProducer) {
+            JobRequestProducer jobRequestProducer, JobSessionRepository jobSessionRepository) {
         this.jobRequestProducer = jobRequestProducer;
+        this.jobSessionRepository = jobSessionRepository;
     }
 
     @PostMapping("/async")
-    public ResponseEntity<CompileResponse> compile(CompileRequest req) throws JsonProcessingException {
-        JobMessage job = new JobMessage(UUID.randomUUID(), req.getSourceCode());
-        jobRequestProducer.send(job);
+    public ResponseEntity<CompileResponse> compile(
+            @CookieValue("${app.constants.user-id-attribute}") UUID userId,
+            CompileRequest req) throws JsonProcessingException {
 
-        CompileResponse res = new CompileResponse(job);
-        return ResponseEntity.ok(res);
+        JobMessage job = new JobMessage(UUID.randomUUID(), req.getSourceCode());
+        
+        jobRequestProducer.send(job);
+        jobSessionRepository.save(
+                new JobSession(job.getId(), userId));
+
+        return ResponseEntity.ok(new CompileResponse(job));
     }
 }
--- backend/src/main/java/org/eduscript/services/impl/JobRequestProducerImpl.java
+++ backend/src/main/java/org/eduscript/services/impl/JobRequestProducerImpl.java
@@ -1,6 +1,7 @@
 package org.eduscript.services.impl;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.eduscript.model.JobMessage;
 import org.eduscript.services.JobRequestProducer;
@@ -35,15 +36,31 @@ public class JobRequestProducerImpl implements JobRequestProducer {
     @Override
     public void send(JobMessage job) throws JsonProcessingException {
         String jobStr = objectMapper.writeValueAsString(job);
-        
-        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(
-                jobRequestsTopic, job.getId().toString(), jobStr);
-        future.whenComplete((result, ex) -> {
-            if (ex != null) {
-                logger.error("An error occurred while sending data to topic", ex);
-            } else {
-                logger.debug("Sent data to topic %s successfully: %s", jobRequestsTopic, jobStr);
-            }
-        });
+
+        // CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(
+        // jobRequestsTopic, job.getId().toString(), jobStr);
+        // future.whenComplete((result, ex) -> {
+        // if (ex != null) {
+        // logger.error("An error occurred while sending data to topic", ex);
+        // } else {
+        // logger.debug("Sent data to topic %s successfully: %s", jobRequestsTopic,
+        // jobStr);
+        // }
+        // });
+
+        try {
+            kafkaTemplate
+                    .send(jobRequestsTopic, job.getId().toString(), jobStr)
+                    .get(); // Blocks until send completes
+
+            logger.debug("Sent data to topic {} successfully: {}", jobRequestsTopic, jobStr);
+        } catch (InterruptedException e) {
+            logger.error("Kafka send was interrupted", e);
+            throw new RuntimeException("Kafka send interrupted", e);
+        } catch (ExecutionException e) {
+            logger.error("Failed to send data to topic", e.getCause());
+            throw new RuntimeException("Kafka send failed", e.getCause());
+        }
+
     }
 }
